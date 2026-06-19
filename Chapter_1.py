3d9cb57b683bd2bf5134098c8ce5baca26dc18bc
Samyog249 <ghimiresamyog249@gmail.com>
2026-06-19 18:58:18 +0545

    Chapter 1

---
+++ Chapter_1.py
@@ -0,0 +1,34 @@
+print("===== Comments =====")
+#This is called Comments in this the interpreter will ignore this line and in python we use (#) for Single line Comments.
+"""This 
+is Multiline Comment
+in Python we use (""" """) for MultiLine Comments"""
+
+print("====Variables====")
+"""The name which is used to give for data type is called Variables in our case a,Samyog,
+Kathmandu are Variables.There are some rule for when we define Variables they are we can't use
+special Characters as a variables."""
+a = 20
+Samyog = 30 
+Kathmandu = 20
+#defining variables in this way without breaking rule if we break rule error comes in program
+Samyog_Ghimire = 19 # this is snake case  
+SamyogGhimire = 40  # this is pascal case 
+samyogGhimire = 33 # this is camel case
+print(a)
+print(Samyog)
+print(Kathmandu)
+print(Samyog_Ghimire)
+print(SamyogGhimire)
+print(samyogGhimire)
+
+"""Pythons built-in data types classify values and determine the operations possible on them. Since everything in Python is an object
+these types are implemented as classes, and variables are instances of these classes.
+
+1. Numeric Types
+
+int: Arbitrary-precision integers.
+
+float: Double-precision floating-point numbers.
+
+complex: Numbers with real and imaginary parts (a + bj)."""
